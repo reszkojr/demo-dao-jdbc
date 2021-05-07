76367ad06558d2dbee3f21e77ae290fe56a018ec
santores <s.reszkojr@gmail.com>
2021-05-07 18:38:25 -0300

    Seller entity class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 
 import model.Department;
+import model.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department dp = new Department(1, "Books");
+        Seller sl = new Seller(1, "Amando", "amando@amando.amando", new Date(), 3000.0, dp);
         System.out.println(dp);
+        System.out.println(sl);
     }
 }
