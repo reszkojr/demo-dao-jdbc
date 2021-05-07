7b21a36c68657399655d3eea37606130d0ef2a29
santores <s.reszkojr@gmail.com>
2021-05-07 18:29:04 -0300

    Department entity class

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import model.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department dp = new Department(1, "Books");
+        System.out.println(dp);
+    }
+}
---
+++ src/model/Department.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String toString() {
+        return this.id + ", " + this.name;
+    }
+
+}
